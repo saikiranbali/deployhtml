50f9c3810ab71a93ad1027ab9c0f2a329a011985
saikiranbali <95091708+saikiranbali@users.noreply.github.com>
2022-11-18 16:39:15 +0530

    -

--- app.py
+++ app.py
@@ -16,10 +16,22 @@ def index():
     # create a cursor
 
     cur = conn.cursor()
-    cur.execute("select * from inventory")
+    cur.execute("select * from inventory WHERE sku LIKE 'BLR%'")
     data = cur.fetchall()
+
+    cur1 = conn.cursor()
+    cur1.execute("select * from inventory WHERE sku LIKE 'MUM%'")
+    data1 = cur1.fetchall()
+
+    cur2 = conn.cursor()
+    cur2.execute("select * from inventory WHERE sku LIKE 'HYD%'")
+    data2 = cur2.fetchall()
+
+    cur3 = conn.cursor()
+    cur3.execute("select * from inventory WHERE sku LIKE 'CHN%'")
+    data3 = cur3.fetchall()
     print(data)
-    return render_template('index.html', data=data)
+    return render_template('index.html', data=data, data1=data1, data2=data2, data3=data3)
 if __name__ == '__main__':
     app.run(debug=True)
 
